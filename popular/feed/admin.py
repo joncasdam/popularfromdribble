9652b4b6ca9f319a6acaf78b479103aaa3e269df
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-01-13 17:59:12 -0200

    ajusta admin de Entrada

--- popular/feed/admin.py
+++ popular/feed/admin.py
@@ -1,6 +1,14 @@
+# -*- encoding: utf-8 -*-
+
 from django.contrib import admin
+from imagekit.admin import AdminThumbnail
 
 from .models import Player, Entrada
 
+class EntradaAdmin(admin.ModelAdmin):
+    miniatura = AdminThumbnail(image_field='thumb_admin')
+    list_display = ['title', 'miniatura']
+
+
 admin.site.register(Player)
-admin.site.register(Entrada)
+admin.site.register(Entrada, EntradaAdmin)
