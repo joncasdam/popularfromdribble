d60e4c027f7529dd55ff791c2b366c39c3af2a3c
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-01-13 11:30:26 -0200

    ajustes no settings

--- popular/popular/settings.py
+++ popular/popular/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'feed',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,8 +60,8 @@ WSGI_APPLICATION = 'popular.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
     }
 }
 
