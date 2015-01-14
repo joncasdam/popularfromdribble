8af79b8f90a5eee925c300488b95595fac8146ad
Jonatas C Damasceno <jonatas.cd@gmail.com>
2015-01-14 04:28:12 -0200

    ajusta views da app feed

--- popular/feed/views.py
+++ popular/feed/views.py
@@ -1,7 +1,17 @@
 # -*- encoding: utf-8 -*-
 
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
 
+from.utils import fetch_popular_from_dribble
+from .models import Entrada
 
 def home(request):
-    return render(request, 'home.html')
+    dados = fetch_popular_from_dribble()
+    contexto = {'shots': dados.get('shots', None)}
+    return render(request, 'home.html', contexto)
+
+def shot(request, shot_id):
+    shot = get_object_or_404(Entrada, dribble_id=shot_id)
+    return render(request, 'shot.html', shot.to_dict())
